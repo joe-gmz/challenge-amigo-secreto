a22fd721d9974c578d79a50749084164d3a2a46e
Joel Gomez <joel.gomeza99@gmail.com>
2025-08-14 22:32:17 -0500

    Agrega función sortearAmigo y mustra el resultado aleatorio

--- app.js
+++ app.js
@@ -10,10 +10,9 @@ function agregarAmigo (nombre) {
      }
      ActualizarListaAmigos();
      limpiarNombre();
-     listaDeAmigos();
-     return amigos;
-}
 
+}
+// Actualiza la lista de amigos
 function ActualizarListaAmigos () {
      let listaAmigos = document.getElementById('amigo');
      amigos.push(listaAmigos.value);
@@ -21,32 +20,8 @@ function ActualizarListaAmigos () {
      return amigos;
 }
 
+// Limpia el input luego de agregar un amigo
 function limpiarNombre () {
      document.querySelector("#amigo").value = "";
      return;
 }
-
-function listaDeAmigos () {
-     let lista = document.getElementById('listaAmigos');
-     lista.innerHTML = "";
-     amigos.forEach(function (amigo) {
-          let li = document.createElement('li');
-          li.textContent = amigo;
-          lista.appendChild(li);
-     });
-     return amigos;
-}
-
-function sortearAmigo () {
-     if (amigos.length === 0) {
-          alert("Debes agregar al menos un amigo para sortear");
-          return;
-     }
-     let seleccionarAmigoAleaorio = Math.floor(Math.random() * amigos.length);
-     let amigoSeleccionado = amigos[seleccionarAmigoAleaorio];
-     console.log(seleccionarAmigoAleaorio);
-     console.log(amigoSeleccionado);
-     document.getElementById("resultado").innerHTML = amigoSeleccionado;
-}
-
-sortearAmigo();
